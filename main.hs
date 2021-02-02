148b8d71d612435f08d7440b6f853c37dfcab4d0
Kenny F <kennyjesus@protonmail.com>
2021-02-02 11:34:38 +0100

    Add dataset

---
+++ main.hs
@@ -0,0 +1,18 @@
+-- El programa desarrollado debe ser completamente funcional y compilable con la versión de
+-- Haskell 8.6. Si se usa alguna librería no empleada en la asignatura, indicar en la memoria cual y
+-- cómo instalarla con cabal.
+-- Para la superación del trabajo, el código debe ser programación funcional y contener
+-- como mínimo, de forma natural:
+-- • Dos usos de cada concepto básico de programación funcional visto en la asignatura. Es
+-- decir: al menos usar 2 funciones básicas de prelude y Data.List, definir 2 funciones
+-- recursivas, definir 2 funciones por patrones, 2 usos de guardas, 2 usos de case of, 2 usos
+-- de listas por comprensión, 2 usos de orden superior, declaraciones de tipos para todas las
+-- funciones definidas, 2 usos de evaluación perezosa, etc.
+-- • Creación de un módulo
+-- • Creación de dos tipos de datos nuevos y usos de éstos.
+-- • Uso de al menos dos de tipos de datos abstractos o librerías vistos en la asignatura (por
+-- ejemplo, pilas, colas, map, matrix, array).
+
+
+main :: IO()
+main =  putStrLn "Hello World"
